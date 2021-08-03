36a03e3bdfcd81c61697dc9da55b7d42578e53c2
MyHanhYuuki <MyHanhPhung0302@gmail.com>
2021-08-04 00:33:41 +0700

    New webAPIs

---
+++ QLNH_APIs/QLNH_APIs/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLNH_APIs.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
